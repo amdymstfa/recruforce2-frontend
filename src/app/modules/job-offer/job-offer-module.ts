25b24eee03131645d1e79239f08fe95b92622da7
amdymstfa <amdymoustapha011@gmail.com>
2025-12-09 12:21:32 +0100

    Initilize modules (lazy-loaded)

---
+++ src/app/modules/job-offer/job-offer-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JobOfferRoutingModule } from './job-offer-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    JobOfferRoutingModule
+  ]
+})
+export class JobOfferModule { }
