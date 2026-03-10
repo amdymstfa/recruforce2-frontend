a6d572b4871a000572ca29900ca34db1de96c58e
amdymstfa <amdymoustapha011@gmail.com>
2026-03-10 01:59:37 +0000

    feat: add environment configuration files for dev and prod

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  aiServiceUrl: 'http://localhost:8000',
+  n8nWebhookUrl: 'http://localhost:5678/webhook'
+};
